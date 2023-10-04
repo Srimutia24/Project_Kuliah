af83b603590ec5e15ab3dbf2ea57cef123b56390
srimutia24 <srimutia2121@gmail.com>
2023-10-04 14:11:02 +0700

    praktikum

---
+++ praktikum 4/form.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form method="post" action="proses.php">
+        <lable>Nilai 1</lable>
+        <input type="text" name="nilai1"></br>
+        <lable>Nilai 2</lable>
+        <input type="text" name="nilai2"></br>
+
+        <input type="submit" name="submit" value="Proses">
+</form>
+        
+</body>
+</html>
---
+++ praktikum 4/proses.php
@@ -0,0 +1,8 @@
+<?php
+
+$nilai1=$_POST['nilai1'];
+$nilai2=$_POST['nilai2'];
+//echo $_POST['nilai1']+$_POST['nilai2'];
+echo "Angka $nilai1 ditambah angka $nilai2 adalah ".$nilai1+$nilai2
+
+?>
