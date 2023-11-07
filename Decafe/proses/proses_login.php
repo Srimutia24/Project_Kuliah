1b1f47c9d795a1e23163444e43c53f1b7e597323
srimutia24 <srimutia2121@gmail.com>
2023-11-07 09:17:36 +0700

    update login

--- Decafe/proses/proses_login.php
+++ Decafe/proses/proses_login.php
@@ -1,38 +1,22 @@
 <?php
 session_start();
 include "connect.php";
-$id = (isset($_POST['id'])) ? htmlentities($_POST['id']) : " ";
-$passwordlama = (isset($_POST['passwordlama'])) ? md5(htmlentities($_POST['passwordlama'])) : " ";
-$passwordbaru = (isset($_POST['passwordbaru'])) ? md5(htmlentities($_POST['passwordbaru'])) : " ";
-$repasswordbaru = (isset($_POST['repasswordbaru'])) ? md5(htmlentities($_POST['repasswordbaru'])) : " ";
 
-if (!empty($_POST['ubah_password_validate'])) {
-    $query = mysqli_query($conn, "SELECT * FROM tb_user WHERE username = '$_SESSION[username_decafe]' && password = '$passwordlama' ");
+$username = (isset($_POST['username'])) ?  htmlentities($_POST['username']) : "";
+$password = (isset($_POST['password'])) ? md5(htmlentities($_POST['password'])) : "";
+if (!empty($_POST['submit_validate'])) {
+    $query = mysqli_query($conn, "SELECT * FROM tb_user WHERE username = '$username' && password = '$password'");
     $hasil = mysqli_fetch_array($query);
     if ($hasil) {
-        if ($passwordbaru == $repasswordbaru) {
-            $query = mysqli_query($conn, "UPDATE tb_user SET password='$passwordbaru'WHERE username = '$_SESSION[username_decafe]'");
-            if ($query) {
-                $message = '<script>alert("Password Berhasil Diubah")
-                    window.history.back()</script>
-                     </script>';
-            } else {
-                $message = '<script>alert("Password Gagal Diubah")
-     window.history.back()</script>
-      </script>';
-            }
-        } else {
-            $message = '<script>alert("Password baru tidak sama")
-     window.history.back()</script>
-      </script>';
-        }
-    } else {
-        $message = '<script>alert("Password lama tidak sesuai")
-     window.history.back()</script>
-      </script>';
+        $_SESSION['username_decafe'] = $username;
+        $_SESSION['level_decafe'] = $hasil['level'];
+        header('location:../home');
+    } else { ?>
+        <script>
+            alert('Username atau password yang anda masukkan salah');
+            window.location = '../login'
+        </script>
+<?php
     }
-} else {
-    header('location:../home');
 }
-echo $message;
 ?>
